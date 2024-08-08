63449a90ec7e8d04822a83cf2f8bb556af4c50d5
SouRS <SouRS@SOURSPC>
2024-08-08 14:15:14 +0800

    Добавьте файлы проекта.

---
+++ Калькулятор1/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace Калькулятор1
+{
+    public partial class Form1 : Form
+       
+    {
+        float result,summ;
+        string oper,num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            textBox1.Text = textBox1.Text+'1';
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + '2';
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + '3';
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + '4';
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + '5';
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + '6';
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + '7';
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + '8';
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + '9';
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+         
+            textBox1.Text = textBox1.Text + "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                result = Convert.ToSingle(textBox1.Text);
+                textBox1.Text = "";
+            }
+            oper = "+";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+         
+            if (textBox1.Text != "")
+            {
+            result = Convert.ToSingle(textBox1.Text);
+            textBox1.Text = "";
+            }
+            oper = "-";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                result = Convert.ToSingle(textBox1.Text);
+                textBox1.Text = "";
+            }
+            oper = "/";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                result = Convert.ToSingle(textBox1.Text);
+                textBox1.Text = "";
+            }
+            oper = "*";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked ==true)
+            {
+                
+                this.BackColor = Color.Black;
+                checkBox1.Text = "Светлая тема";
+                checkBox1.ForeColor = Color.White;
+            } else
+            {
+                this.BackColor = Color.White;
+                checkBox1.Text = "Темная тема";
+                checkBox1.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            summ = 0;
+            if ( oper == "+") 
+            {
+                num = textBox1.Text;
+                summ = result+float.Parse(textBox1.Text) ;
+                textBox1.Text = Convert.ToString(summ);
+                textBox2.Text = textBox2.Text+  result+oper +num + '='+Convert.ToString(summ)+'\r' + '\n';
+            }
+            if (oper == "-")
+            {
+                num = textBox1.Text;
+                summ = result - float.Parse(textBox1.Text);
+                textBox1.Text = Convert.ToString(summ);
+                textBox2.Text = textBox2.Text + result + oper + num + '=' + Convert.ToString(summ) + '\r' + '\n';
+            }
+            if (oper == "*")
+            {
+                num = textBox1.Text;
+                summ = result * float.Parse(textBox1.Text);
+                textBox1.Text = Convert.ToString(summ);
+                textBox2.Text = textBox2.Text + result + oper + num + '=' + Convert.ToString(summ) + '\r' + '\n';
+            }
+            if (oper == "/")
+            {
+
+                num = textBox1.Text;
+                if (float.Parse(textBox1.Text) == 0)
+                {
+                    MessageBox.Show("Делить на 0 нельзя!", "Внимание!");
+                        
+                    
+                }else { 
+                summ = result / float.Parse(textBox1.Text);
+                textBox1.Text = Convert.ToString(summ);
+                textBox2.Text = textBox2.Text + result + oper + num + '=' + Convert.ToString(summ) + '\r' + '\n';
+            }
+            }
+
+        }
+    }
+}
---
+++ Калькулятор1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Калькулятор1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Калькулятор1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cf2a8c86-cd53-4e12-9052-6d5d5b242336")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
